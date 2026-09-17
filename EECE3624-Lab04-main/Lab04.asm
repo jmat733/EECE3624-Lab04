702d9f3fac96fcaa6dc5e9240895c2df45bdd25c
jmat733 <jtmatthews@mail.lipscomb.edu>
2026-09-16 22:10:07 -0500

    pushing

--- EECE3624-Lab04-main/Lab04.asm
+++ EECE3624-Lab04-main/Lab04.asm
@@ -20,7 +20,7 @@ main:       ; jump here on reset
 		ldi R16, low(RAMEND)
 		out SPL, R16
 
-		LDI  n, 4	; load a value into n
+		LDI  n, 5	; load a value into n
 		PUSH n	; push it on the stack
 		CALL factN	; calculate the factorial of n
 		POP  result	; pop result off stack
@@ -29,14 +29,38 @@ here:
 
 factN:
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-	; Comments regarding the factN subroutine go here
+	; factN recusivly computes n!
+	; 1. caller pushes n onto the stack
+	; 2. caller runs CALL factN
+	; 3. factN writes over the same stack byte with (n!)
+	; 4. caller retrieves answer with pop
 	;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 	; recursive factorial code begins here
+	IN YL, SPL ; low byte loading y
+	IN YH, SPH ; high byte loading y
+	LDD R18, Y+3 ; R18 will be n
 
+	CPI R18, 1 ; base case where n = 1
+	BRNE recursiveCase
+
+	LDI R18, 1 ; where base case goes to
+	STD Y+3, R18 ; overwriting our n with result
+	ret
 
 
 recursiveCase:
+	DEC R18 ; now R18 = n - 1
+	PUSH R18 ; pushing n - 1 (but not my n)
 
+	CALL factN
+
+	POP R19 ; which is (n-1)! and brings back SP
+	IN YL, SPL ; reloading y
+	IN YH, SPH ; reloading y
+	LDD R18, Y+3 ; reloading my n
+	MUL R18, R19 ; multiplying for factorial
+	STD Y+3, R0 ; low byte back into n
+	
 
 	ret 
 	; return from the factN subroutine
